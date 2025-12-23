aa53dbc269df9125b5df6338def11991a43c6efb
Jake Hathaway <jake.d.hathaway@gmail.com>
2025-12-22 16:54:40 -0800

    add data table component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "@/index.css";
 import App from "@/app";
 import { BrowserRouter } from "react-router-dom";
 
