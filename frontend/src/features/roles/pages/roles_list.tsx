54f750891bcc3a2c543f8e673a453ddaf02482ec
Jake Hathaway <jake.d.hathaway@gmail.com>
2025-12-22 16:54:40 -0800

    add data table component

--- frontend/src/features/roles/pages/roles_list.tsx
+++ frontend/src/features/roles/pages/roles_list.tsx
@@ -1,6 +1,31 @@
 import { useRoleStore } from "@/features/roles/stores/role_store";
 import { useEffect } from "react";
 import type { Role } from "../types";
+import type { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "@/components/ui/data_table";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+
+const columns: ColumnDef<Role>[] = [
+  {
+    accessorFn: (item) => item.id,
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+];
 
 export default function RolesList() {
   const initializeRoleStore = useRoleStore((state) => state.initialize);
@@ -10,11 +35,5 @@ export default function RolesList() {
     initializeRoleStore();
   }, [initializeRoleStore]);
 
-  return (
-    <ul>
-      {roles.map((role) => (
-        <li>{role.name}</li>
-      ))}
-    </ul>
-  );
+  return <DataTable columns={columns} data={roles} filterColumnName="name" />;
 }
